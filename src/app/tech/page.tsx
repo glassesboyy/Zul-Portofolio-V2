774b6b6d1eafc1abff819a8675c3759cf35fb00b
glassesboyy <aliperwira26@gmail.com>
2025-05-12 05:12:36 +0700

    first commit, navbar, animasi perpindahan tiap page

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -1,10 +1,10 @@
 import { Template } from "@/components/template";
 
-export default function Home() {
+export default function Tech() {
   return (
     <Template>
       <div className="min-h-screen bg-black flex flex-col items-center justify-center p-4">
-        <h1 className="text-4xl font-bold">Welcome to Home Page</h1>
+        <h1 className="text-4xl font-bold">Welcome to Tech Page</h1>
       </div>
     </Template>
   );
